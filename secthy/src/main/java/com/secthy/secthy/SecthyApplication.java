cbc337d774d635722a73bcdee8237f42b77990a4
“Luca-Muratori” <luca.muratori.94@hotmail.it>
2023-05-04 23:55:38 +0200

    almost finish section5

---
+++ secthy/src/main/java/com/secthy/secthy/SecthyApplication.java
@@ -0,0 +1,13 @@
+package com.secthy.secthy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecthyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecthyApplication.class, args);
+	}
+
+}
---
+++ secthy/src/test/java/com/secthy/secthy/SecthyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secthy.secthy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecthyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
